ff630cc99fbb63bb82ec48e12738cf5b20ab34c9
Louiefigz <figueroa.aaron@gmail.com>
2016-07-22 13:47:44 -0400

    Updating some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,14 @@
 @post_one = Post.create(name: "first post ever", content: "This is the first post ever made", user_id: @one.id)
 @post_two = Post.create(name: "Shepards pie", content: "Grandma is the best", user_id: @two.id)
 @post_three = Post.create(name: "winning the championship", content: "It ain't easy man. Forget what they told you.", user_id: @three.id)
+
+
+@tag_one = Tag.create(name: "Humor")
+@tag_two = Tag.create(name: "Education")
+@tag_three = Tag.create(name: "Topical")
+@tag_four = Tag.create(name: "Rant")
+
+
+@post_one.tags << [@tag_one, @tag_two]
+@post_two.tags << [@tag_three, @tag_four]
+@post_three.tags << [@tag_one, @tag_four, @tag_three]
