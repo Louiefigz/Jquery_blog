81025a464a79a3c8969b1b2303a7d474b866dc05
Louiefigz <figueroa.aaron@gmail.com>
2016-10-07 20:36:20 -0400

    minor deletes

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -9,17 +9,16 @@ if (path.split('/').slice(-1) == "edit"){
 
 if ( path.includes("/posts") && path.split('/')[path.split('/').length-1] !== "" ){
   $.getJSON(path).done(function(response){
-
     showPagePost(response.post);
-    if (response.post.user.id !== response.post.current_user_id){
-      $('.editPage').hide();
-    }
-    // debugger;
+      if (response.post.user.id !== response.post.current_user_id){
+        $('.editPage').hide();
+      }
     $('.editNameForm').find("input[type='text']").attr("value", response.post.name);
     $('#edit-content').val(response.post.content);
   });
 }
 
+// POST CONTENT
 var showPagePost= function(post){
   $('#post-name').html(post.name);
   $('#post-content').html(post.content);
@@ -27,15 +26,14 @@ var showPagePost= function(post){
   showComments(post);
 }
 
+  //DELETE POST
 function deleteButton(path){
-
-  var deletehtml =
-  '<a  data-method="delete" href=" '+ path+ '" class="myButton">' + 'Delete Post' + '</a>' + '<br>' + '<br>';
+  var deletehtml = '<a  data-method="delete" href=" '+ path+ '" class="myButton">' + 'Delete Post' + '</a>' + '<br>' + '<br>';
   return deletehtml;
 }
 
 
-
+//COMMENTS
 var showComments = function(post){
   var dom ="";
   post.comments.forEach(function(comment){
@@ -47,48 +45,44 @@ var showComments = function(post){
 }
 
 var showComment = function(comment){
-  // debugger;
   var d = new Date(comment.created_at);
-  // console.log(comment.created_at);
-
-
-    var comment_td =
+  var comment_td =
     '<div>' +
-    '<div class="media">'+
-        '<a class="pull-left" href="#">'+
-            '<img class="media-object" src="http://placehold.it/64x64" alt="">' +
-        '</a>' +
+      '<div class="media">'+
+          '<a class="pull-left" href="#">'+
+              '<img class="media-object" src="http://placehold.it/64x64" alt="">' +
+          '</a>' +
 
-        '<div class="media-body" >' +
-            '<h4 class="media-heading" data-name=" ' + comment.content + ' " data-post-id=" ' + comment.id +' ">' + "Start Bootstrap" +
-                '<small>' + d + '</small>' +
-            '</h4>' +
-            comment.content
-        '</div>' +
+      '<div class="media-body" >' +
+          '<h4 class="media-heading" data-name=" ' + comment.content + ' " data-post-id=" ' + comment.id +' ">' + "Start Bootstrap" +
+              '<small>' + d + '</small>' +
+          '</h4>' +
+          comment.content
+      '</div>' +
     '</div>';
 
        comment_td += showReplies(comment.replies);
        comment_td +=
-        '<br>' +
+       '<br>' +
+         '<table>' +
+             '<tr>' +
+               '<td>' +
+                 '<form id="createCommentReplyForm">'+
+                   '<input type="text" placeholder="Comment Reply" class="new-reply" data-commentId="' + comment.id + '"  required>' +
+                   '<input type="hidden" value="'+comment.id+'" id="parent_id">' +
+                   '<input type="hidden" value="'+comment.post_id+'" id="post_id">' +
+                   '<input type="submit">' +
+                 '</form>' +
+               '</td>' +
+             '</tr>' +
+           '</table>' +
+          '</div>' + '<br>';
 
-       '<table>' +
-       '<tr>' +
-       '<td>' +
-       '<form id="createCommentReplyForm">'+
-         '<input type="text" placeholder="Comment Reply" class="new-reply" data-commentId="' + comment.id + '"  required>' +
-         '<input type="hidden" value="'+comment.id+'" id="parent_id">' +
-         '<input type="hidden" value="'+comment.post_id+'" id="post_id">' +
-         '<input type="submit">' +
-       '</form>' +
-       '</td>' +
-       '</tr>';
-
-       comment_td += '</table>' + '</div>' + '<br>';
   return comment_td;
 }
 
 function showReplies(comments){
-var replyComment="";
+  var replyComment="";
   comments.forEach(function(reply){
     replyComment+= showReply(reply);
   });
@@ -96,20 +90,18 @@ var replyComment="";
 }
 
 function showReply(reply){
-  // debugger;
   var d = new Date(reply.created_at);
   var replyComment =
 
-  '<div class="media">'+
-      '<a class="pull-left" href="#">' +
-          '<img class="media-object" src="http://placehold.it/64x64" alt="">' +
-      '</a>' +
-      '<div class="media-body">' +
-          '<h4 class="media-heading">' + "Nested Start Bootstrap" +
-              '<small>' + d + '</small>' +
-          '</h4>' +
-          reply.content +
-      '</div>' +
-  '</div>' ;
+    '<div class="media">'+
+        '<a class="pull-left" href="#">' +
+            '<img class="media-object" src="http://placehold.it/64x64" alt="">' +
+        '</a>' +
+        '<div class="media-body">' +
+            '<h4 class="media-heading">' + "Nested Start Bootstrap" +
+                '<small>' + d + '</small>' +
+            '</h4>' + reply.content +
+        '</div>' +
+    '</div>' ;
 return replyComment;
 }
