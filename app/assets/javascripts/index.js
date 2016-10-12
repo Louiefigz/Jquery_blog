70cdb871183c1d586cab670176c0bc43b5166050
Louiefigz <figueroa.aaron@gmail.com>
2016-10-12 15:27:44 -0400

    placed my old changes back. I remembered I was creating Javascript Objects for this page. I also took away code that was no longer being in use

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -20,8 +20,15 @@ function getAllPosts(){
 
 // This function is being used after an ajax request and no page refresh
 function reloadPost(path){
+  $.getJSON(path).done(function(response){
 
+    // Instead of returning an json object, I decided to make it a javascript object
+    // in order to make it easier to organize my posts from everyone else's
+    createPostObjects(response);
+    showPosts(posts);
     getAllPosts();
+
+    //This is clearing the form on the index page
     $('#mypostnumber').html($('#myposts .post-listener').length);
     $('#create-post input[type=text]').val('');
     $('#create-post textarea').val('');
@@ -29,8 +36,11 @@ function reloadPost(path){
     // deleteTag()
     $('#new-post-form').show();
 
+    // posts = [];
+  });
 }
 
+//Creating the Javascript Object
 var createPostObjects = function(response) {
   posts =[]
   myPosts =[]
@@ -38,6 +48,7 @@ var createPostObjects = function(response) {
   response.posts.forEach(function(post) {
     posts.push(new Post(post.id, post.name, post.content, post.user.id, post.current_user_id))
   })
+
   showPosts(posts);
   showMyPosts();
   showRestPosts();
@@ -54,12 +65,11 @@ var Post = function(id, name, content, user_id, current_user_id) {
 
 Post.prototype.my_post = function(){
 
- if(this.current_user_id == this.user_id) {
-   myPosts.push(this);
-  } else {
+   if (this.current_user_id == this.user_id) {
+     myPosts.push(this);
+   } else {
      restPosts.push(this);
    }
-
  }
 
 
@@ -67,15 +77,6 @@ Post.prototype.my_post = function(){
 function showRestPosts(){
   var dom = "";
   restPosts.forEach(function(post) {
-    // var post_td =
-    // '<table>'+
-    // '<tr>'+
-    // '<td class="post-listener" data-name=" ' + post.name + ' " data-post-id=" ' + post.id +' ">'+
-    //  post.name +'</td>'+
-    //
-    // '<td>'+  '<a href="/posts/'+post.id+' " >' + "Show " + " "+' </td>' +
-    //     '<tr>' + '<table>';
-
     dom += (showPost(post, post.current_user_id));
   });
 
@@ -86,27 +87,11 @@ function showRestPosts(){
 function showMyPosts(){
   var dom = "";
   myPosts.forEach(function(post) {
-
-
-    // var post_td =
-    // '<table>'+
-    // '<tr>'+
-    // '<td class="post-listener" data-name=" ' + post.name + ' " data-post-id=" ' + post.id +' ">'+
-    //  post.name +'</td>'+
-    // //  console.log(post.id);
-    // '<td>'+  '<a href="/posts/'+post.id+' " >' + "Show " + " "+' </td>' +
-    //
-    //     '<td>'+  '<a href="/posts/'+post.id+'/edit " >' + " Edit" +' </td>'+
-    //     '<td>'+  '<a data-method="delete" href="/posts/'+post.id+' " >' + " Destroy" +'</td>'+
-    //     '<tr>' + '<table>';
-
-
     dom += (showPost(post, post.current_user_id));
   });
 
   $('#myposts table').remove();
   $("#myposts").html(dom);
-
 }
 
 
@@ -121,10 +106,7 @@ var showPosts = function(posts) {
 }
 
 function showPost(post, current_user_id) {
-
   var content = post.content.slice(0, 200);
-  // debugger ;
-  // return $('<li>', {'data-name': tag.name, 'data-tagid': tag.id, text: tag.name  });
   var post_td =
   '<h2>' +
       '<a href="/posts/'+post.id+' " >' + post.name + '</a>' +
@@ -142,31 +124,3 @@ function showPost(post, current_user_id) {
 
   return post_td;
 }
-
-// '<table>'+
-// '<tr>'+
-// '<td class="post-listener" data-name=" ' + post.name + ' " data-post-id=" ' + post.id +' ">'+
-//  post.name +'</td>'+
-// //  console.log(post.id);
-// '<td>'+  '<a href="/posts/'+post.id+' " >' + "Show " + " "+' </td>';
-//
-//   if (post.user_id == post.current_user_id) {
-//     post_td +=
-//       '<td>'+  '<a href="/posts/'+post.id+'/edit " >' + " Edit" +' </td>'+
-//       '<td>'+  '<a data-method="delete" href="/posts/'+post.id+' " >' + " Destroy" +'</td>';
-//   }
-
-              // '<h2>' +
-              //     '<a href="#">' + 'Blog Post Title'+ '</a>' +
-              // '</h2>' +
-              // '<p class="lead">' +
-              //     'by' +  '<a href="index.php">' + 'Start Bootstrap' + '</a>' +
-              // '</p>' +
-              // '<p>' + '<span class="glyphicon glyphicon-time">' + '</span>' + 'Posted on August 28, 2013 at 10:45 PM' + '</p>' +
-              // '<hr>' +
-              // '<img class="img-responsive" src="http://placehold.it/900x300" alt="">' +
-              // '<hr>' +
-              // '<p>' + "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!" + '</p>' +
-              // '<a class="btn btn-primary" href="#">' + "Read More" +  '<span class="glyphicon glyphicon-chevron-right">' + '</span>' + '</a>' +
-              //
-              // '<hr>';
